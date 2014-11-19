3845a9efa15c40c1725f6b6fe4776c1b4b9f74fb
SanCoder-Q <sancoder.q@gmail.com>
2014-11-19 12:19:41 +0800

    Fix a bug of printing date info in payment.html

--- coffee/main.js
+++ coffee/main.js
@@ -5,4 +5,5 @@ shoppingCartApp = angular.module('shoppingCartApp', []);
 shoppingCartApp.controller('ShoppingCartCtrl', (function($scope) {
   $scope.items = loadAllItems();
   $scope.shoppingCart = new ShoppingCart();
+  $scope.date = utils.getDate;
 }));
